762cd5f40392f0a05607f41fbffc0564b2190281
Joenil Tingson <joenil098890@gmail.com>
2025-09-16 10:20:32 +0800

    make to crawl all pages

--- src/main.js
+++ src/main.js
@@ -94,7 +94,7 @@ const crawler = new PlaywrightCrawler({
   },
 
   // Let's limit our crawls to make our tests shorter and safer.
-  maxRequestsPerCrawl: 5,
+ // maxRequestsPerCrawl: 5,
 });
 
 try {
