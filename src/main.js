b559122043da911d4857c53065719b8351cb51fe
Joenil Tingson <joenil098890@gmail.com>
2025-09-17 12:22:02 +0800

    fixed company age

--- src/main.js
+++ src/main.js
@@ -119,7 +119,7 @@ const crawler = new PlaywrightCrawler({
       const registrationNumber = await getTextOrNull(page.locator('h3:has-text("General Information") + p.mt-1'));
       const address = await getTextOrNull(page.locator('dt:has-text("Registered Address") + dd.mt-1 a'));
       const status = await getTextOrNull(page.locator('dt:has-text("Operating Status") + dd.mt-1'));
-      const companyAge = await getTextOrNull(page.locator('dt.text-sm:has-text("Company Age") + dd.mt-1'));
+      const companyAge = await getTextOrNull(page.locator('dt:has-text("Company Age") + dd'));
 
       const primaryssic = await getTextOrNull(page.locator('dt:has-text("Primary SSIC Code") + dd.mt-1 a'));
       const primaryIndustry = await getTextOrNull(page.locator('dt:has-text("Primary Industry") + dd.mt-1 a'));
@@ -196,7 +196,7 @@ const crawler = new PlaywrightCrawler({
   },
 
   // Let's limit our crawls to make our tests shorter and safer.
-  maxRequestsPerCrawl: 15,
+  maxRequestsPerCrawl: 5,
 });
 
 try {
