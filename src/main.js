d05919d59367fe106f6f0dc532234f7cac984f91
Joenil Tingson <joenil098890@gmail.com>
2025-09-17 14:33:32 +0800

    remove unecessary

--- src/main.js
+++ src/main.js
@@ -126,48 +126,11 @@ const crawler = new PlaywrightCrawler({
       const registrationNumber = await getTextOrNull(page.locator('h3:has-text("General Information") + p.mt-1'));
       const address = await getTextOrNull(page.locator('dt:has-text("Registered Address") + dd.mt-1 a'));
       const status = await getTextOrNull(page.locator('dt:has-text("Operating Status") + dd.mt-1'));
-      // Building Name
-      let buildingName = null;
-      try {
-        buildingName = await getTextOrNull(page.locator('dt:has-text("Building") + dd.mt-1'));
-      } catch (e) {
-        buildingName = null;
-      }
-
-      // Contact Number
-      let contactNumberString = null;
-      try {
-        const contactNumberElement = page
-          .locator('dt:has-text("Contact Number") + dd.mt-1')
-          .filter({
-              hasText: '+',
-          })
-          .first();
-        if (await contactNumberElement.count() > 0) {
-          contactNumberString = await contactNumberElement.textContent();
-          contactNumberString = contactNumberString ? contactNumberString.trim() : null;
-        }
-      } catch (e) {
-        contactNumberString = null;
-      }
-
-      // Company Email
-      let companyEmail = null;
-      try {
-        companyEmail = await getTextOrNull(page.locator('dt:has-text("Email") + dd.mt-1 a'));
-      } catch (e) {
-        companyEmail = null;
-      }
-
-      // Company Website
-      let companyWebsite = null;
-      try {
-        companyWebsite = await getTextOrNull(page.locator('dt:has-text("Website") + dd.mt-1 a'));
-      } catch (e) {
-        companyWebsite = null;
-      }
-
-      const companyAge = await getTextOrNull(page.locator('div#overview dt:has-text("Company Age") + dd'));
+      //const buildingName = await getTextOrNull(page.locator('dt:has-text("Building") + dd.mt-1'));
+      //const contactNumberString = await getTextOrNull(page.locator('dt:has-text("Contact Number") + dd.mt-1'));
+      //const companyEmail = await getTextOrNull(page.locator('dt:has-text("Email") + dd.mt-1 a'));
+      //const companyWebsite = await getTextOrNull(page.locator('dt:has-text("Website") + dd.mt-1 a'));
+      // const companyAge = await getTextOrNull(page.locator('div#overview dt:has-text("Company Age") + dd'));
     
 
       const primaryssic = await getTextOrNull(page.locator('dt:has-text("Primary SSIC Code") + dd.mt-1 a'));
@@ -183,11 +146,11 @@ const crawler = new PlaywrightCrawler({
           "Registration Number": registrationNumber,
           "Address": address,
           "Status": status,
-          "Company Age": companyAge,
-          "Building Name": buildingName,
-          "Contact Number": contactNumberString,
-          "Company Email": companyEmail,
-          "Company Website": companyWebsite
+          // "Company Age": companyAge,
+          // "Building Name": buildingName,
+          // "Contact Number": contactNumberString,
+          // "Company Email": companyEmail,
+          // "Company Website": companyWebsite
         },
         "Industry Classification" : {
           "Primary SSIC Code": primaryssic,
