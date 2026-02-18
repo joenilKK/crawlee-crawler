83dd57e22177a4a99eacc03a6b9ad8bc506ba8c2
Joenil Tingson <joenil098890@gmail.com>
2026-02-18 13:44:20 +0800

    added date discovered

--- src/main.js
+++ src/main.js
@@ -77,6 +77,7 @@ const crawler = new PlaywrightCrawler({
         email,
         address,
         language,
+        dateDiscovered: new Date().toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' }),
       };
 
 
